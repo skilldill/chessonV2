52b6aa351d86e0e2186f9f03ebedb18f747d69b9
Alexander <skilldill@yandex.ru>
2025-10-01 22:54:56 +0300

    Added base for websocket

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,6 @@
 import { Elysia } from 'elysia'
 import { connectDB } from './config/database'
+import { websocket } from 'elysia/ws';
 
 // Connect to MongoDB
 connectDB()
@@ -11,3 +12,13 @@ app.get('/api/health', () => ({
   status: 'ok',
   timestamp: new Date().toISOString()
 }));
+
+app.ws('/ws', {
+  message: (ws, message) => {
+    ws.send('OK this is read' + JSON.stringify(message));
+  },
+});
+
+const port = process.env.PORT ? parseInt(process.env.PORT) : 4000
+console.log(`Server is running on port ${port}`);
+app.listen(port)
--- site_experimental/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import Component from "../landing-page"
-
-export default function Page() {
-  return <Component />
-}
