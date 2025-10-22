fb8d4a23ab2f2d93943a67f7f11562af4bf299f0
Alexander <skilldill@yandex.ru>
2025-10-22 23:25:04 +0300

    Update gradient color in HistoryMoves component for improved visual consistency

--- client/src/components/HistoryMoves/HistoryMoves.tsx
+++ client/src/components/HistoryMoves/HistoryMoves.tsx
@@ -37,7 +37,7 @@ export const HistoryMoves: FC<HistoryMovesProps> = ({ moves }) => {
                 }}
             >
                 <div className="relative w-full h-full p-[8px] bg-back-secondary rounded-xl overflow-hidden flex flex-col justify-end transition-transform duration-200 active:scale-95">
-                    <div className="absolute top-0 left-0 right-0 h-[98px] bg-gradient-to-t from-zink-950 to-black" />
+                    <div className="absolute top-0 left-0 right-0 h-[98px] bg-gradient-to-t from-zink-950 to-[#0a0a0a]" />
                     {groupedMoves.map((moveItem, index) => (
                         <div className={styles.movesRow}>
                             <div key={`${moveItem[0]}_${index}`} className="flex items-center gap-x-[8px] px-[4px] py-[6px] hover:bg-white/4 transition-all duration-200 cursor-pointer grid grid-cols-[40px_1fr_1fr]">
