da43b804de9c00fc2c8abf2f9cc9c0a489ecfab7
Alexander <skilldill@yandex.ru>
2025-11-16 12:15:55 +0300

    Add sitemap integration and update site configuration
    - Integrated @astrojs/sitemap for improved SEO and site indexing.
    - Updated site URL in astro.config.mjs.
    - Added Open Graph image meta tag for better social media sharing.
    - Created robots.txt to guide web crawlers.
    - Added og.png for Open Graph image.

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -5,15 +5,19 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://chesson.me',
+
   vite: {
     plugins: [
       tailwindcss()
     ]
   },
 
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 
   i18n: {
     locales: ["en", "ru"],
